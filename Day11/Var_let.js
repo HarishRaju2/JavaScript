8f07858f3170b189ad4fb9fb009fce830df325c8
HarishRaju2 <169036798+HarishRaju2@users.noreply.github.com>
2024-05-20 16:59:24 +0530

    Add files via upload

---
+++ Day11/Var_let.js
@@ -0,0 +1,27 @@
+x="Var block";
+console.log(x);
+ 
+ function let_ch()
+{
+    var a=4;
+    for(var a=0;a<=4;a++)
+    {
+        console.log(a);
+    }
+    console.log(a);
+}
+let_ch();
+
+x="Let block";
+console.log(x);
+
+ function let_ch()
+{
+     let b=5;
+    for(let b=0;b<=8;b++)
+    {
+        console.log(b);
+    }
+    console.log(b);
+}
+let_ch();
