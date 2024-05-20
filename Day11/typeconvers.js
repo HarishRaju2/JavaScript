23f9e8b33643f42932670177fc4a9c4b05529725
HarishRaju2 <169036798+HarishRaju2@users.noreply.github.com>
2024-05-20 16:56:25 +0530

    Create typeconvers.js

---
+++ Day11/typeconvers.js
@@ -0,0 +1,25 @@
+top="Implicit converstion"
+console.log(top)
+console.log("_______________________________________-")
+let name="harish"/2;
+let num="34"+46;
+console.log(num)
+console.log(name)
+
+top1="Explicit converstion"
+console.log(top1)
+console.log("_______________________________________-")
+let a=Number("3421")
+console.log(typeof(a))
+
+let b=Number("harish")
+console.log(typeof(b))
+
+let c=String("3421")
+console.log(typeof(c))
+
+let alive=Number(true)
+console.log(typeof(alive))
+
+let nul=Boolean(null)
+console.log(nul)
