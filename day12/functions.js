65b57d5d4e687047b7cfcf6958411083df5dd364
HarishRaju2 <169036798+HarishRaju2@users.noreply.github.com>
2024-05-21 15:00:42 +0530

    Add files via upload

---
+++ day12/functions.js
@@ -0,0 +1,27 @@
+function let()
+{
+    let a='harish'
+    console.log(a);
+
+}
+let()
+
+function vars()
+{
+    var num=10;
+    for(let num=0;num<6;num++)
+        {
+            console.log(num)
+
+        }                            
+}
+vars()
+
+function display(name,age,gender)
+{
+    console.log(name)
+    console.log(age)
+    console.log(gender)
+
+}
+display('harish',21,'M')
