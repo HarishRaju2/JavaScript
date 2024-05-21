901c5a9855df55f5f427f33052a1bc05db529728
HarishRaju2 <169036798+HarishRaju2@users.noreply.github.com>
2024-05-21 10:46:18 +0530

    Create if_else.js

---
+++ day12/if_else.js
@@ -0,0 +1,29 @@
+let name="harish"
+let rollno=21
+let seat=3
+
+if(rollno>0&&rollno<10)
+    {
+        console.log(name+" Your Exam Hall number is 201 and seat no in Third Row")
+    }
+    else if(rollno>10&&rollno<20)
+        {
+            console.log(name+" your Exam HAll Number is 202 and seat no in Frist Row")
+        }
+    else if(rollno>20&&rollno<30)
+        {
+            console.log(name+" your Exam HAll Number is 203 and seat no in Second Row")
+        }
+    else if(rollno>30&&rollno<40)
+        {
+            console.log(name+" your Exam HAll Number is 204 and seat no in Fourth Row")
+        }
+    else if(rollno>40&&rollno<50)
+        {
+            console.log(name+" your Exam HAll Number is 205 and seat no in Fivth Row")
+        }
+    else
+        {
+            console.log("Check your RollNo")
+
+        }
