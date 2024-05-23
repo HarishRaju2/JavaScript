ec26376c742ce5bc670f9a0c7b4460de03204c43
HarishRaju2 <169036798+HarishRaju2@users.noreply.github.com>
2024-05-23 09:47:33 +0530

    Add files via upload

---
+++ Day13/construct_function.js
@@ -0,0 +1,10 @@
+function Kec(university,state,district,location)
+{
+    this.university=university;
+    this.state=state;
+    this.district=district;
+    this.location=location;
+
+}
+const myclg=new Kec("Anna University","Tamil Nadu","Erode","Perundurai")
+console.log(myclg);
