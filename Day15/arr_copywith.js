b4977c7659f086dd767c5d42871bb585187e71b0
HarishRaju2 <169036798+HarishRaju2@users.noreply.github.com>
2024-05-24 17:49:54 +0530

    Add files via upload

---
+++ Day15/arr_copywith.js
@@ -0,0 +1,14 @@
+names=[
+    "Anand",
+    "Bala",
+    "Chandru",
+    "Dinesh"]
+    
+console.log('Normal Array values')
+
+console.log(names)
+
+x=names.copyWithin(0,1);
+
+console.log("By Using CopyWithin Method")
+console.log(x)
