f891e9e7f7c0eab8e3d14ef2170696dd3db70a01
HarishRaju2 <169036798+HarishRaju2@users.noreply.github.com>
2024-05-27 11:30:27 +0530

    Add files via upload

---
+++ Day11/explicit_convers.js
@@ -0,0 +1,22 @@
+top1="Explicit converstion"
+console.log(top1)
+console.log("_______________________________________-")
+let a=Number("3421")
+console.log(a)
+console.log(typeof(a))
+
+let b=Number("harish")
+console.log(b)
+console.log(typeof(b))
+
+let c=String("3421")
+console.log(c)
+console.log(typeof(c))
+
+let alive=Number(true)
+console.log(alive)
+console.log(typeof(alive))
+
+let nul=Boolean(null)
+console.log(nul)
+console.log(nul)
