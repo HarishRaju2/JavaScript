39dee9ea9b14e0cfd6264abfd08177fb4915e193
HarishRaju2 <169036798+HarishRaju2@users.noreply.github.com>
2024-05-27 16:49:04 +0530

    Create map_fun.js

---
+++ Day16/map_fun.js
@@ -0,0 +1,10 @@
+let  val=[2,5,7,13];
+let sample=val.map((val)=>val*2)
+console.log(sample)
+
+const val_1=new Map()
+val_1.set('Butterscotch','venilla','Mango','Double chocolate')
+
+console.log(val_1.has('venilla'))
+console.log(val_1.get('Butterscotch'))
+console.log(val_1.delete('venilla'))
