b7deb8dce625e55f248ad68e3effce4a211a4003
HarishRaju2 <169036798+HarishRaju2@users.noreply.github.com>
2024-05-27 16:50:13 +0530

    Add files via upload

---
+++ Day16/str_methods_4.js
@@ -0,0 +1,43 @@
+function upper()
+{
+    let str="harish";
+    res=str.toUpperCase();
+    console.log(res)
+}
+upper()
+
+function lower()
+{
+    let str_1="ABIJITH";
+    res_1=str_1.toLowerCase();
+    console.log(res_1)
+}
+lower()
+
+function trim()
+{
+    let str_2='       Welcome Back';
+    console.log(str_2);
+    res_2=str_2.trim()
+    console.log(res_2)
+}
+trim()
+
+
+function trim_start()
+{
+    let str_3=' -      Javascript    -';
+    console.log(str_3);
+    res_3=str_3.trimStart()
+    console.log(res_3)
+}
+trim_start()
+
+function trim_end()
+{
+    let str_4=' -      Javascript    -';
+    console.log(str_4);
+    res_4=str_4.trimEnd()
+    console.log(res_4)
+}
+trim_end()
