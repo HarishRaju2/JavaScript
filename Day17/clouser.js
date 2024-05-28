06f40a5915f88d0cc333991ab19240a4ccd1b5d1
HarishRaju2 <169036798+HarishRaju2@users.noreply.github.com>
2024-05-28 17:47:53 +0530

    Create clouser.js

---
+++ Day17/clouser.js
@@ -0,0 +1,34 @@
+function car()
+{
+
+    let name='Hyundai'
+    let model='Asta'
+    let type='petrol'
+    let color='Black'
+
+    function properties()
+    {
+        let rating=4.5
+        let safety=4.5
+        let air_bag=4
+
+        console.log(" OF CAR")
+
+        console.log(`Name of car ${name}`)
+        console.log(`Model of car ${model}`) 
+
+        console.log(`Fuel Type for car ${type}`)
+        console.log(`Color of the car ${color}`)
+
+        console.log("****SPECIFICATION  OF CAR****")
+        console.log(`Rating of the car ${rating}`)
+
+        console.log(`Safety Rating ${safety}`)
+        console.log(`Air Bag Availability ${air_bag}`)
+    }
+
+    properties()
+
+}
+
+car()
